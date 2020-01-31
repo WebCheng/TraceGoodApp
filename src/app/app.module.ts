56de884fc50dda3c2423a7a76d0442d7c297e65a
Chen Zhuoling <32111322+chenz6@users.noreply.github.com>
2020-01-31 14:31:32 -0800

    add the login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule,Routes} from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+
+const appRoutes:Routes=[
+  // {path:'',redirectTo:'Home',pathMatch:'full'},
+  // {path:'Home',component: HomeComponent}, 
+  // {path:'Contact',component: ContactComponent},
+ 
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
