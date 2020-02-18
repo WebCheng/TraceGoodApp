94e4f77801806e84142d8e8fb09da410df39fe34
Chen Zhuoling <32111322+chenz6@users.noreply.github.com>
2020-02-18 10:54:56 -0800

    logup data

--- src/app/pages/logup/logup.component.ts
+++ src/app/pages/logup/logup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-logup',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogupComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  logupForm: FormGroup;
+  submitted = false;
+  
+  constructor( 
+  	private formBuilder: FormBuilder
+  	
+  	) 
+  {     
+  
   }
 
+  ngOnInit() {
+
+  	this.logupForm = this.formBuilder.group({
+      email:['', Validators.compose([Validators.required])],
+     
+    });
+  }
+
+
+  onSubmit() {
+    this.submitted = true;
+
+    }
+
 }
+
+
+
+
+
+
+
+
+// export class SignUpComponent implements OnInit {
+//   signupForm: FormGroup;
+//   result = false;
+//   invalidsignup = false;
+//   submitted = false;
+
+
+//   ngOnInit() {
+//   }
+
+//   onSubmit() {
+//     this.submitted = true;
+
+//     if (this.signupForm.invalid) {
+//       console.log(this.signupForm.value);
+//       return;
+//     }
+
+//     this.userService.signup(this.signupForm.value).pipe(first()).subscribe(
+//       data=>{data.toString();
+//       if(data) {
+//         this.router.navigate(['/login']);
+//       };
+//       if(!data){
+//         this.invalidsignup = true;
+//       };
+//       },
+//       error => {console.log('error')}
+//       );
+//   }
+// }
